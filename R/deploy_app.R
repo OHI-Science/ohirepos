c36a1442a7890b75202309265a23069846a2c7ba
bbest <bdbest@gmail.com>
2016-09-29 11:29:16 -0700

    deploy_app() @import -> NAMESPACE, app.brew.yaml mod, inst/shiny_app -> inst/app for https://github.com/OHI-Science/issues/issues/710

--- R/deploy_app.R
+++ R/deploy_app.R
@@ -2,31 +2,39 @@
 #'
 #' @param gh_repo Github repository.
 #' @param gh_owner Github owner. Defaults to "OHI-Science"
+#' @param gh_branch_data Github branch containing data. Defaults to "draft" and must already exist in the repo.
+#' @param gh_branch_app Github branch containing data. Defaults to "app" and does not have to already exist in the repo.
 #' @return Returns URL of Shiny app if successfully deployed, otherwise errors out.
 #' @examples
 #' \dontrun{
 #' deploy_app("bhi")
 #' }
-#' @import git2r
+#' @import tidyverse devtools brew
 #' @export
-deploy_app <- function(gh_repo, gh_owner='OHI-Science', gh_branch_data='draft', gh_branch_app='app', debug=F){
-  # gh_repo='bhi'; gh_owner='OHI-Science'; gh_branch_data='draft'; gh_branch_app='app'; debug=F
+deploy_app <- function(
+  gh_repo, gh_owner='OHI-Science',
+  gh_branch_data='draft', gh_branch_app='app',
+  app_url=sprintf('http://ohi-science.nceas.ucsb.edu/%s', gh_repo),
+  debug=F){
 
   library(tidyverse)
-  library(git2r)
+
+  # library(devtools); load_all(); debug=T; gh_repo='bhi'; gh_owner='OHI-Science'; gh_branch_data='draft'; gh_branch_app='app'; app_url=sprintf('http://ohi-science.nceas.ucsb.edu/%s', gh_repo)
 
   # Copied from https://github.com/OHI-Science/ohi-webapps/blob/723ded3a6e1cfeb0addb3e8d88a3ccf1081daaa3/create_functions.R#L1045-L1116
 
+  # old ----
   #key <<- key # assign key to the global namespace so available for other functions
   #source(file.path(dir_github, 'ohi-webapps/create_init_sc.R'))
-
   # delete old
   #dir_app_old <- sprintf('%s/git-annex/clip-n-ship/%s/shinyapps.io', dir_neptune, git_repo)
   #unlink(dir_app_old, recursive=T)
 
+  # new ----
+
   # use temporary directory
   if (debug){
-    tmpdir = '/var/folders/pj/l9cfhbn97xbcgqx6qyx0lr800000gn/T//RtmpnHDDDD/'
+    tmpdir = '/var/folders/pj/l9cfhbn97xbcgqx6qyx0lr800000gn/T//Rtmpuwembr'
   } else {
     tmpdir = tempdir()
   }
@@ -35,16 +43,20 @@ deploy_app <- function(gh_repo, gh_owner='OHI-Science', gh_branch_data='draft', 
   dir_branches = sprintf('%s/%s'   , tmpdir, gh_repo)
   dir_data     = sprintf('%s/%s/%s', tmpdir, gh_repo, gh_branch_data)
   dir_app      = sprintf('%s/%s/%s', tmpdir, gh_repo, gh_branch_app)
-  gh_url = sprintf('https://github.com/%s/%s.git', gh_owner, gh_repo)
+  gh_slug      = sprintf('%s/%s', gh_owner, gh_repo)
+  gh_url       = sprintf('https://github.com/%s.git', gh_slug)
+
+  # extra bash commands
+  rm_allnotgit = 'find . -path ./.git -prune -o -exec rm -rf {} \\; 2> /dev/null'
 
   # delete existing, if not in debug mode
   if (!debug){
-    if (file.exists(dir_repo)) unlink(dir_branches, recursive = T)
-    dir.create(dir_branches)
-  }
+    # clear existing
+    if (file.exists(dir_branches)) unlink(dir_branches, recursive = T)
 
-  # clone data branch, shallowly and quietly
-  system(sprintf('git clone --quiet --depth 1 --branch %s %s %s', gh_branch_data, gh_url, dir_data))
+    # clone data branch, shallowly and quietly
+    system(sprintf('git clone --quiet --depth 1 --branch %s %s %s', gh_branch_data, gh_url, dir_data))
+  }
 
   # get remote branches
   remote_branches = read_tsv(
@@ -61,38 +73,46 @@ deploy_app <- function(gh_repo, gh_owner='OHI-Science', gh_branch_data='draft', 
 
     # create orphan app branch
     system(sprintf(
-      'cp -rf %s %s; cd %s; git checkout --orphan app; rm -rf *; touch README.md; git add README.md; git commit -m "initialize app branch"',
-      dir_data, dir_app, dir_app))
+      'cp -rf %s %s; cd %s; git checkout --orphan %s; rm -rf *; touch README.md; git add README.md; git commit -m "initialize %s branch"',
+      dir_data, dir_app, dir_app, gh_branch_app, gh_branch_app))
 
   } else {
 
-    # clear existing app branch
+    # clone app branch, clear existing files
     system(sprintf(
-      'cp -rf %s %s; cd %s; git checkout app; rm -rf *',
-      dir_data, dir_app, dir_app))
+      'git clone --quiet --depth 1 --branch %s %s %s; cd %s; rm -rf *', gh_branch_app, gh_url, dir_app, dir_app))
+
   }
 
-  # copy installed ohicore shiny app files
-  # good to have latest dev ohicore first:
-  devtools::install_github('ohi-science/ohicore@dev')  # update by JSL March 19. Could cause problems since need to make sure pulled latest version
-  dir_ohicore_app = '~/github/ohicore/inst/shiny_app' #file.path(system.file(package='ohicore'), 'shiny_app') #
-  shiny_files = list.files(dir_ohicore_app, recursive=T)
-  for (f in shiny_files){ # f = shiny_files[1]
-    dir.create(dirname(f), showWarnings=F, recursive=T)
-    suppressWarnings(file.copy(file.path(dir_ohicore_app, f), f, overwrite=T, recursive=T, copy.mode=T, copy.date=T))
+  # copy shiny app files into dir_app
+  suppressWarnings(file.copy(system.file('app', package='ohirepos'), dir_branches, recursive = T))
+
+  # TODO: continue below updating templates ----
+
+  # get commit of ohirepos for Shiny app provenance
+  ohirepos_commit <- devtools:::local_sha('ohirepos')
+  if (nchar(ohirepos_commit) != 40){
+    stop(sprintf(paste(
+      'Sorry, the ohirepos R library seems to have not been installed with:',
+      '  devtools::install_github("ohi-science/ohirepos")',
+      'based on devtools:::local_sha("ohirepos") of %s and not of normal Github commit length 40,',
+      'which is necessary to associate the ohirepos commit with the Shiny app deployed.', collapse='\n'), ohirepos_commit))
   }
+  # TODO: OLD -> NEW variable assignment
+  # git_owner  = gh_owner       # 'jules32', #g[['GithubUsername']],   ## generalize-- DESCRIPTION not found...
+  # git_repo   = gh_repo        # 'gye', # g[['GithubRepo']],
+  # git_branch = gh_branch_data # 'draft', # g[['GithubRef']],
+  # git_commit = gh_commit      # 'initial commit')) #g[['GithubSHA1']]))
 
-  # get commit version of ohicore app files
-  lns = readLines(file.path(dir_ohicore_app, '../../DESCRIPTION'))
-  g = sapply(str_split(lns[grepl('^Github', lns)], ': '), function(x) setNames(x[2], x[1]))
-  #ohicore_app_commit = sprintf('%s/%s@%s,%.7s', g[['GithubUsername']], g[['GithubRepo']], , g[['GithubSHA1']])
-  ohicore_app = list(ohicore_app=list(
-    git_owner  = 'jules32', #g[['GithubUsername']],   ## generalize-- DESCRIPTION not found...
-    git_repo   = 'gye', # g[['GithubRepo']],
-    git_branch = 'draft', # g[['GithubRef']],
-    git_commit = 'initial commit')) #g[['GithubSHA1']]))
+  # ohicore_app=list(
+  #   git_owner  = gh_owner,       # 'jules32', #g[['GithubUsername']],   ## generalize-- DESCRIPTION not found...
+  #   git_repo   = gh_repo,        # 'gye', # g[['GithubRepo']],
+  #   git_branch = gh_branch_data, # 'draft', # g[['GithubRef']],
+  #   git_commit = gh_commit,      # 'initial commit')) #g[['GithubSHA1']]))
+  #   ohirepos_commit = ohirepos_commit)
+  # TODO: use ohirepos_commit to stamp which version of ohirepos was used to last modify the shiny app
 
-  brew(file.path(dir_github, 'ohi-webapps/app.brew.yml'), 'app.yml')
+  brew::brew(file.path(dir_app, 'app.brew.yml'), file.path(dir_app, 'app.yml'))
   file.copy(file.path(dir_github, 'ohi-webapps/travis_app.yml'), '.travis.yml') # overwrite=T)
 
   # add Rstudio project files. cannabalized devtools::add_rstudio_project() which only works for full R packages.
