b812694ea4dd8614a50630c1fc89ba6f18e37f8d
bbest <bdbest@gmail.com>
2016-09-29 22:15:24 -0700

    working shiny template with map shrinking param https://github.com/OHI-Science/issues/issues/710

--- R/deploy_app.R
+++ R/deploy_app.R
@@ -1,9 +1,14 @@
 #' Deploy Shiny application
 #'
 #' @param gh_repo Github repository.
-#' @param gh_owner Github owner. Defaults to "OHI-Science"
+#' @param default_scenario The subfolder of the data branch which will be the default scenario of data displayed.
+#' @param app_title The title for the app, which is typically the study area or place name.
+#' @param gh_owner Github owner. Defaults to "OHI-Science".
 #' @param gh_branch_data Github branch containing data. Defaults to "draft" and must already exist in the repo.
-#' @param gh_branch_app Github branch containing data. Defaults to "app" and does not have to already exist in the repo.
+#' @param gh_branch_app Github branch to contain the app. Defaults to "app" and does not have to already exist in the repo.
+#' @param app_url URL of the application
+#' @param debug
+#'
 #' @return Returns URL of Shiny app if successfully deployed, otherwise errors out.
 #' @examples
 #' \dontrun{
@@ -12,14 +17,14 @@
 #' @import tidyverse devtools brew
 #' @export
 deploy_app <- function(
-  gh_repo, gh_owner='OHI-Science',
+  gh_repo, default_scenario, app_title, gh_owner='OHI-Science',
   gh_branch_data='draft', gh_branch_app='app',
   app_url=sprintf('http://ohi-science.nceas.ucsb.edu/%s', gh_repo),
   debug=F){
 
   library(tidyverse)
 
-  # library(devtools); load_all(); debug=T; gh_repo='bhi'; gh_owner='OHI-Science'; gh_branch_data='draft'; gh_branch_app='app'; app_url=sprintf('http://ohi-science.nceas.ucsb.edu/%s', gh_repo)
+  # library(devtools); load_all(); debug=T; gh_repo='bhi'; default_scenario='baltic2015'; app_title='Baltic'; gh_owner='OHI-Science'; gh_branch_data='draft'; gh_branch_app='app'; app_url=sprintf('http://ohi-science.nceas.ucsb.edu/%s', gh_repo)
 
   # Copied from https://github.com/OHI-Science/ohi-webapps/blob/723ded3a6e1cfeb0addb3e8d88a3ccf1081daaa3/create_functions.R#L1045-L1116
 
@@ -30,20 +35,19 @@ deploy_app <- function(
   #dir_app_old <- sprintf('%s/git-annex/clip-n-ship/%s/shinyapps.io', dir_neptune, git_repo)
   #unlink(dir_app_old, recursive=T)
 
-  # new ----
-
   # use temporary directory
   if (debug){
-    tmpdir = '/var/folders/pj/l9cfhbn97xbcgqx6qyx0lr800000gn/T//Rtmpuwembr'
+    tmpdir = '/var/folders/pj/l9cfhbn97xbcgqx6qyx0lr800000gn/T//Rtmpp9mCyh'
   } else {
     tmpdir = tempdir()
   }
 
   # construct vars
-  dir_branches = sprintf('%s/%s'   , tmpdir, gh_repo)
-  dir_data     = sprintf('%s/%s/%s', tmpdir, gh_repo, gh_branch_data)
-  dir_app      = sprintf('%s/%s/%s', tmpdir, gh_repo, gh_branch_app)
-  gh_slug      = sprintf('%s/%s', gh_owner, gh_repo)
+  dir_branches = file.path(tmpdir, gh_repo)
+  dir_data     = file.path(tmpdir, gh_repo, gh_branch_data)
+  dir_app      = file.path(tmpdir, gh_repo, gh_branch_app)
+  dir_data_2   = file.path(tmpdir, gh_repo, gh_branch_app, gh_branch_data)
+  gh_slug      = file.path('%s/%s', gh_owner, gh_repo)
   gh_url       = sprintf('https://github.com/%s.git', gh_slug)
 
   # extra bash commands
@@ -85,12 +89,10 @@ deploy_app <- function(
   }
 
   # copy shiny app files into dir_app
-  suppressWarnings(file.copy(system.file('app', package='ohirepos'), dir_branches, recursive = T))
-
-  # TODO: continue below updating templates ----
+  suppressWarnings(file.copy(system.file('app', package='ohirepos'), dir_branches, recursive=T, overwrite=T))
 
   # get commit of ohirepos for Shiny app provenance
-  ohirepos_commit <- devtools:::local_sha('ohirepos')
+  ohirepos_commit = devtools:::local_sha('ohirepos')
   if (nchar(ohirepos_commit) != 40){
     stop(sprintf(paste(
       'Sorry, the ohirepos R library seems to have not been installed with:',
@@ -112,12 +114,21 @@ deploy_app <- function(
   #   ohirepos_commit = ohirepos_commit)
   # TODO: use ohirepos_commit to stamp which version of ohirepos was used to last modify the shiny app
 
-  brew::brew(file.path(dir_app, 'app.brew.yml'), file.path(dir_app, 'app.yml'))
-  file.copy(file.path(dir_github, 'ohi-webapps/travis_app.yml'), '.travis.yml') # overwrite=T)
+  # brew app.yml configuration and introduction files, and delete brew templates
+  brew(system.file('app/app.brew.yml', package='ohirepos'), file.path(dir_app, 'app.yml'))
+  brew(system.file('intro.brew.md'   , package='ohirepos'), file.path(dir_app, 'intro.md'))
+  unlink(sprintf('%s/%s', dir_app, c('app.brew.yml','intro.brew.md')))
 
-  # add Rstudio project files. cannabalized devtools::add_rstudio_project() which only works for full R packages.
-  file.copy(system.file('templates/template.Rproj', package='devtools'), sprintf('%s.Rproj', key))
-  writeLines(c('.Rproj.user', '.Rhistory', '.RData', 'github', git_repo), '.gitignore')
+  # TODO: Travis?
+  #brew::brew(system.file('app/travis.brew.yml', package='ohirepos'), file.path(dir_app, 'travis.yml'))
+
+  # add Rstudio project and gitignore files
+  file.copy(system.file('templates/template.Rproj', package='devtools'), sprintf('%s/%s.Rproj', dir_app, gh_repo))
+  writeLines(c('.Rproj.user', '.Rhistory', '.RData', gh_branch_data), file.path(dir_app, '.gitignore'))
+
+  # TODO finish up from 2016-09-29.... ----
+
+  # TODO next: move dir_data to dir_data_2
 
   # shiny::runApp()    # test app locally
 
