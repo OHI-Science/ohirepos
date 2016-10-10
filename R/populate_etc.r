70d0068952c189ec2c14c1a16f42f9c434146f3a
jules32 <jstewart@nceas.ucsb.edu>
2016-10-10 11:20:31 -0700

    adding roxygen2

--- R/populate_etc.r
+++ R/populate_etc.r
@@ -1,12 +1,23 @@
 # populate_etc.r
 
+#' Populate other items in OHI repos
+#'
+#' @param key OHI assessment identifier, e.g. 'gye' for 'Gulf of Guayaquil'
+#' @param dir_scenario full path of temporary OHI repo and scenario, e.g. `~/github/clip-n-ship/gye`
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#'
 populate_etc <- function(key, dir_scenario) {
-  
+
   ## calculate_scores.r:: copy ## TODO: make this a template. ----
   fn <- 'calculate_scores.r'
   file.copy(file.path('~/github/ohi-webapps/inst', fn),
             file.path(dir_scenario, fn), overwrite=TRUE)
-  
+
   ## calculate_scores.r:: update source()
   readLines(file.path(dir_scenario, fn)) %>%
      str_replace("ohi-global/eez[0-9]{4}", file.path(key, default_scenario)) %>%
@@ -16,24 +27,24 @@ populate_etc <- function(key, dir_scenario) {
   fn <- 'configure_toolbox.r'
   file.copy(file.path('~/github/ohi-webapps/inst', fn),
             file.path(dir_scenario, fn), overwrite=TRUE)
-  
+
   ## configure_toolbox.r:: update setwd()
   readLines(file.path(dir_scenario, fn)) %>%
     str_replace("ohi-global/eez[0-9]{4}", file.path(key, default_scenario)) %>%
     writeLines(file.path(dir_scenario, fn))
-  
-  
+
+
   ## copy install_ohicore.r
   fn <- 'install_ohicore.r'
-  file.copy(file.path('~/github/ohi-webapps/inst', fn), 
+  file.copy(file.path('~/github/ohi-webapps/inst', fn),
             file.path(dir_repo, fn), overwrite=TRUE)
-  
- 
+
+
   ## copy temp/referencePoints.csv
   fn <- 'referencePoints.csv'
   dir_temp = file.path(dir_scenario, 'temp')
   dir.create(dir_temp)
-  file.copy(file.path('~/github/ohi-webapps/inst', fn), 
+  file.copy(file.path('~/github/ohi-webapps/inst', fn),
             file.path(dir_temp, fn), overwrite=TRUE)
-  
+
 }
