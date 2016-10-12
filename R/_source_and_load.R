fe763a665762345b925042e857c09c353a0e24c3
jules32 <jstewart@nceas.ucsb.edu>
2016-10-12 08:54:59 -0700

    a bit of roxygen

--- R/_source_and_load.R
+++ R/_source_and_load.R
@@ -1,5 +1,5 @@
 ## _source_and_load.r
-# temporary file to load/source all the scripts necessary for the ohirepos package. 
+# temporary file to load/source all the scripts necessary for the ohirepos package.
 
 ## R scripts
 to_source <- list.files('~/github/ohi-webapps/R', all.files=TRUE, full.names=TRUE)[-c(1,2)]
@@ -15,6 +15,3 @@ if (Sys.info()[['sysname']] != 'Linux' & !file.exists(dir_M)){
   warning(sprintf("The Mazu directory dir_M set in src/R/common.R does not exist. Do you need to mount Mazu: %s?", dir_M))
 }
 
-# libraries
-library(tidyverse)
-library(rgdal)
--- R/clone_repo.r
+++ R/clone_repo.r
@@ -1,12 +1,22 @@
 # clone_repo.r
 
+#' Clone existing repo
+#'
+#' @param dir_repos
+#' @param dir_repo
+#' @param git_url
+#'
+#' @return
+#' @export
+#'
+#' @examples
 clone_repo <- function(dir_repos, dir_repo, git_url) {
-  
+
   ## clone repo
   unlink(dir_repo, recursive=T, force=T)
   repo = clone(git_url, normalizePath(dir_repo, mustWork=F))
-  
+
   ## get remote branches
   remote_branches = sapply(git2r::branches(repo, 'remote'), function(x) str_split(x@name, '/')[[1]][2])
-  
+
 }
