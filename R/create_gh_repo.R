d62124005c2d4d3958b2dbb879edbdd00ba4cc24
jules32 <jstewart@nceas.ucsb.edu>
2017-07-10 13:05:50 -0700

    in progress: update create_gh_repo.R

--- R/create_gh_repo.R
+++ R/create_gh_repo.R
@@ -10,23 +10,68 @@
 #' @examples
 create_gh_repo <- function(key, gh_token, github_user){
 
-  repo_name <- key
-  cmd <- sprintf('git ls-remote git@github.com:ohi-science/%s.git', repo_name)
-  res <- system(cmd, ignore.stderr=T, intern=T)
-  repo_exists <- ifelse(length(res)!=0, T, F)
+  ## Create a temporary directory to hold the repository
+  path <- path.expand(file.path('~/github/clip-n-ship', key))
+  dir.create(path)
+  
+  ## clone existing repo (put warning and waiting message)
+  repo <- git2r::clone(url = sprintf('https://github.com/OHI-Science/%s', key),
+                       local_path = path)
+  
+#   
+  # ## Initialize the repository
+  # repo <- git2r::init(path)
 
-  ## create and run command call using Github API: https://developer.github.com/v3/repos/#create
-  if (!repo_exists){
-    message(sprintf('%s: creating github repo -- %s', repo_name, format(Sys.time(), '%X')))
+#   ## Display a brief summary of the new repository
+#   repo
+#   
+#   ## Config user and commit a file
+#   git2r::config(repo, user.name="jules32", user.email="lowndes@nceas.ucsb.edu")
+#   
+#   
+#   git2r::is_bare(repo)
+#   git2r::is_empty(repo)
+#   
+  writeLines("Hello world!", file.path(path, "test.txt"))
 
-    ## create command
-    cmd <- sprintf('curl --silent -u "%s:%s" https://api.github.com/orgs/ohi-science/repos -d \'{"name":"%s"}\'',
-                   github_user, gh_token, repo_name)
-    ## run command
-    cmd_res = paste(capture.output(fromJSON(system(cmd, intern=T))), collapse='\n')
-
-  } else{
-    message(sprintf('%s: repo already exists.', repo_name))
-
-  }
-}
+  ## Add file and commit
+  git2r::add(repo, "test.txt")
+  git2r::commit(repo, "Commit README")
+#   
+#   git2r::branches(repo)
+ # git2r::branch_get_upstream(head(repo))
+#   
+  git2r::push(repo) #, "master", "origin/master")
+#   
+#   
+#   ## from push help BOO
+#   # path_bare <- file.path('~/github/clip-n-ship/bare')
+#   # path_repo <- file.path('~/github/clip-n-ship', key)
+#   # dir.create(path_bare)
+#   # dir.create(path_repo)
+#   # repo_bare <- git2r::init(path_bare, bare = TRUE)
+#   # repo <- git2r::clone(path_bare, path_repo)
+#   
+#   
+#   ## ORIG BELOW
+#   
+#   
+#   cmd <- sprintf('git ls-remote git@github.com:ohi-science/%s.git', key)
+#   res <- system(cmd, ignore.stderr=T, intern=T)
+#   repo_exists <- ifelse(length(res)!=0, T, F)
+# 
+#   ## create and run command call using Github API: https://developer.github.com/v3/repos/#create
+#   if (!repo_exists){
+#     message(sprintf('%s: creating github repo -- %s', key, format(Sys.time(), '%X')))
+# 
+#     ## create command
+#     cmd <- sprintf('curl --silent -u "%s:%s" https://api.github.com/orgs/ohi-science/repos -d \'{"name":"%s"}\'',
+#                    github_user, gh_token, key)
+#     ## run command
+#     cmd_res = paste(capture.output(jsonlite::fromJSON(system(cmd, intern=T))), collapse='\n')
+# 
+#   } else{
+#     message(sprintf('%s: repo already exists.', key))
+# 
+#   }
+# }
