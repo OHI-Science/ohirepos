0cf21d9326a723d9b82d2213bd46761a30c2a5ab
jules32 <jstewart@nceas.ucsb.edu>
2017-07-19 15:11:35 -0700

    reimagined commit_and_push instead of push_branch

---
+++ R/commit_and_push.r
@@ -0,0 +1,28 @@
+#' commit_and_push
+#'
+#' @param key OHI assessment identifier, e.g. 'gye' for 'Gulf of Guayaquil'
+#' @param dir_repo local directory where you have cloned the repo (probably somewhere temporary)
+#' @param commit_message short message in quotes to accompany commit
+#' @param branch GitHub branch to push, defaults to 'master'
+#'
+#' @return
+#' @export
+#'
+#' @examples
+commit_and_push <- function(key, 
+                            dir_repo, 
+                            commit_message, 
+                            branch = 'master'){
+  
+  ## let us know what's going on
+  cat(sprintf("git add, commit, and push rendered website for %s repo\n", key))
+  cat(sprintf("with commit message '%s'", commit_message))
+  
+  ## cd to dir_repo, git add, commit and push rendered website
+  system.time(system(sprintf(
+    "cd %s; git add --all;
+      git commit -m '%s';
+      git push origin %s",
+    dir_repo, commit_message, branch)))
+  
+}
