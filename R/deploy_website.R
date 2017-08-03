24f1c53c85609641cb251737bad7082bdda7b744
jules32 <jstewart@nceas.ucsb.edu>
2017-08-03 14:22:56 -0700

    brewing website pages and adding MHI's faqs

--- R/deploy_website.R
+++ R/deploy_website.R
@@ -1,6 +1,6 @@
 #' Deploy website to gh-pages branch in Github repository
 #'
-#' @param repo_registry data frame with information about the repo 
+#' @param repo_registry data frame with information about the repo
 #' @param gh_org github organization to place the repo. Default: ohi-science
 #' @param clone T/F: do you want to add, commit, and push? Defaults to TRUE.
 #' @param push T/F: do you want to add, commit, and push? Defaults to TRUE.
@@ -24,8 +24,8 @@ deploy_website <- function(repo_registry,
   dir_scenario_gh <- sprintf(
     "https://raw.githubusercontent.com/%s/%s/master/%s",
     gh_org, key, repo_registry$scenario_name)
-  
-  
+
+
   ## clone existing master branch
   if (clone) {
     unlink(dir_repo, recursive=TRUE, force=TRUE)
@@ -33,21 +33,21 @@ deploy_website <- function(repo_registry,
                                  sprintf('https://github.com/%s/%s.git',
                                          gh_org, key))
   }
-  
+
   ## create empty gh-pages branch
   remote_branches <- git2r::branches(repo)
-  
+
   if ('gh-pages' %in% remote_branches){
-    
+
     ## if gh-pages branch exists, ask if user wants to overwrite
     cat("gh-pages branch already exists, would you like to overwrite it?")
-    
+
   } else {
-    
+
     ## if gh-pages branch does not exist
-    
+
     system(sprintf('cd %s; git checkout --orphan gh-pages;  git rm -rf .', dir_repo))
-    
+
     ## copy gh-pages web files into dir_repo, excluding all files in .gitignore
     system.time(system(
       sprintf(
@@ -56,48 +56,56 @@ deploy_website <- function(repo_registry,
       --include=_footer.html --exclude-from=.gitignore \\
       . %s',
         system.file('gh-pages', package='ohirepos'), dir_repo)))
-    
-    ## brew files
-    brew::brew(system.file('gh-pages/_site.brew.yml', package='ohirepos'),
-               sprintf('%s/_site.yml', dir_repo))
-    brew::brew(system.file('gh-pages/_site.brew.R', package='ohirepos'),
-               sprintf('%s/_site.R', dir_repo))
-    brew::brew(system.file('gh-pages/index.brew.Rmd', package='ohirepos'),
-               sprintf('%s/index.Rmd', dir_repo))
-    brew::brew(system.file('gh-pages/scores.brew.Rmd', package='ohirepos'),
-               sprintf('%s/scores.Rmd', dir_repo))
-    
-    
+
+
+    ## brew this list of files
+    to_brew <- c('_site.brew.yml', '_site.brew.R',
+                 'index.brew.Rmd', 'about.brew.Rmd',
+                 'faq.brew.Rmd',   'scores.brew.Rmd')
+
+    for (tb in to_brew) { # tb <- to_brew[1]
+
+      ## remove brew from filename
+      tb_out <- stringr::str_replace_all(tb, 'brew.', '')
+
+      ## brew!
+      brew::brew(system.file(sprintf('gh-pages/%s', tb), package='ohirepos'),
+                 sprintf('%s/%s', dir_repo, tb_out))
+
+    }
+
+
     ## add Rstudio project file
     file.copy(system.file('templates/template.Rproj', package='devtools'),
               sprintf('%s/%s.Rproj', dir_repo, key))
-    
+
     ## .nojekyll file rmarkdown.rstudio.com/rmarkdown_websites.html#publishing_websites
     system(sprintf('touch %s/.nojekyll', dir_repo))
-    
+
     ## add gitignore file
     writeLines(c(
       '.Rproj.user', '.Rhistory', '.RData', 'rsconnect', '.DS_Store',
       basename(dir_repo)
     ), file.path(dir_repo, '.gitignore'))
-    
+
     ## render website
     rmarkdown::render_site(dir_repo)
-    
+
+
     ## cd to dir_repo, git add, commit and push rendered website
     if (push) {
-      
+
       ## make sure .gitignore and .nojekyll get added
       system(sprintf(
         "cd %s; git add --all; git add .gitignore .nojekyll", dir_repo))
-      
+
       ## commit and push
       ohirepos::commit_and_push(
         repo_registry,
-        commit_message = sprintf("push %s rendered website", key), 
+        commit_message = sprintf("push %s rendered website", key),
         branch = 'gh-pages')
-      
+
     }
-    
+
   }
 }
--- inst/gh-pages/_site.brew.R
+++ inst/gh-pages/_site.brew.R
@@ -8,9 +8,9 @@ suppressPackageStartupMessages({
 })
 
 ## brewed vars
-study_area      = "<%=study_area%>"
-key             = "<%=key%>"
-dir_scenario_gh = "<%=dir_scenario_gh%>"
+study_area      <- "<%=study_area%>"
+key             <- "<%=key%>"
+dir_scenario_gh <- "<%=dir_scenario_gh%>"
 
 ## knitr options for all webpages
 knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
@@ -18,5 +18,6 @@ knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
 ## read in variables
 scores <- readr::read_csv(file.path(dir_scenario, 'scores.csv'))
 layers <- readr::read_csv(file.path(dir_scenario, 'layers.csv'))
-weight <- readr::read_csv(file.path(dir_scenario, 'conf/goals.csv')) %>% select(goal, weight)
+weight <- readr::read_csv(file.path(dir_scenario, 'conf/goals.csv')) %>%
+  select(goal, weight)
 
