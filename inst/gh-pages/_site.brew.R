382c5066276a172f523c7079a84ac09f9b801546
jules32 <jstewart@nceas.ucsb.edu>
2017-08-21 13:49:25 -0700

    rerun with test3 and deploy_websites is working with new goals.rmds

--- inst/gh-pages/_site.brew.R
+++ inst/gh-pages/_site.brew.R
@@ -16,12 +16,12 @@ dir_scenario_gh <- "<%=dir_scenario_gh%>"
 knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
 
 ## read in variables
-scores <- readr::read_csv(file.path(dir_scenario, 'scores.csv'))
-layers <- readr::read_csv(file.path(dir_scenario, 'layers.csv'))
-weight <- readr::read_csv(file.path(dir_scenario, 'conf/goals.csv')) %>%
+scores <- readr::read_csv(file.path(dir_scenario_gh, 'scores.csv'))
+layers <- readr::read_csv(file.path(dir_scenario_gh, 'layers.csv'))
+weight <- readr::read_csv(file.path(dir_scenario_gh, 'conf/goals.csv')) %>%
   select(goal, weight)
 
 ## save local copy of conf/goals.Rmd
-conf_goals <- readr::read_lines(file.path(dir_scenario_gh, 'conf/goals.Rmd'))
+conf_goals <- readr::read_lines(file.path(dir_scenario_gh, 'conf/web/goals.Rmd'))
 readr::write_lines(conf_goals, path = 'conf_goals.Rmd', append = FALSE)
 
