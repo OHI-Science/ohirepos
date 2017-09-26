1028b7ca31e2910161d762ab349d9e9362b618eb
jules32 <jstewart@nceas.ucsb.edu>
2017-09-26 13:22:59 -0700

    add base::system.file so not devtools

--- R/populate_conf.R
+++ R/populate_conf.R
@@ -2,7 +2,7 @@
 #'
 #' Populate OHI repo with configuration folder
 #'
-#' @param repo_registry data frame with information about the repo 
+#' @param repo_registry data frame with information about the repo
 #'
 #' @return key's repo with config folder populated
 #' @export
@@ -19,61 +19,61 @@ populate_conf <- function(repo_registry) {
   dir_scenario_gh <- sprintf(
     "https://raw.githubusercontent.com/%s/%s/master/%s",
     gh_org, key, repo_registry$scenario_name)
-  
-  
+
+
   ## create conf folder
-  
+
   if (!dir.exists(dir_conf)) {
     dir.create(dir_conf, showWarning=FALSE)
   }
 
-  ## list conf files to copy, except functions.r 
+  ## list conf files to copy, except functions.r
   conf_files = c('config.R', 'goals.csv',
                  'pressures_matrix.csv',  'pressure_categories.csv',
                  'resilience_matrix.csv', 'resilience_categories.csv',
-                 'scenario_data_years.csv') 
+                 'scenario_data_years.csv')
 
   for (f in conf_files){ # f = 'config.R'
-    
+
     file.copy(file.path(dir_origin, 'conf', f),
-              file.path(dir_conf, f), 
+              file.path(dir_conf, f),
               overwrite=TRUE)
   }
-  
-  
+
+
   ## copy functions.R from local curated version
-  file.copy(system.file('inst/master/functions_tmp_eez2017.R', 
+  file.copy(base::system.file('inst/master/functions_tmp_eez2017.R',
                         package='ohirepos'),
             file.path(dir_conf, 'functions.R'), overwrite=TRUE)
-  
+
     #   rethink ref points stuff to be useful?
     #   ## write.csv(d_check, sprintf('temp/cs_data_%s.csv', scenario), row.names=FALSE)
     #   s <-  s %>%
     #     str_replace("write.csv\\(tmp, 'temp/.*", '') %>%
     #     str_replace('^.*sprintf\\(\'temp\\/.*', '')
-    # 
+    #
 
 
   ## copy subfolders in goals folder ----
   goal_subfolders <- list.files(system.file('master/web/goals', package='ohirepos'))
   dir_web         <- file.path(dir_conf, 'web')
   dir_goals       <- file.path(dir_conf, 'web/goals')
-  
+
   ## create goals dir if it doesn't already exist
   if(!file.exists(dir_web)) dir.create(dir_web)
   if(!file.exists(dir_goals)) dir.create(dir_goals)
-  
+
   for (g in goal_subfolders){ # g <- "AO.Rmd"
-   
-    file.copy(system.file(sprintf('master/web/goals/%s', g), 
+
+    file.copy(system.file(sprintf('master/web/goals/%s', g),
                           package='ohirepos'),
               file.path(dir_goals, g), overwrite=TRUE)
-    
+
   }
- 
+
   ## brew the parent goals.Rmd file
   brew::brew(system.file(sprintf('master/web/goals.brew.Rmd'), package='ohirepos'),
              file.path(dir_web, 'goals.Rmd'))
-  
+
 
 }
