763d342696d8ea00ceb851a20daf485b2adcdc2f
jules32 <jstewart@nceas.ucsb.edu>
2017-11-15 13:54:46 -0800

    comment

--- inst/gh-pages/_site.brew.R
+++ inst/gh-pages/_site.brew.R
@@ -20,6 +20,7 @@ scores_csv <- file.path(dir_scenario_gh, 'scores.csv')
 layers_csv <- file.path(dir_scenario_gh, 'layers.csv')
 conf_csv   <- file.path(dir_scenario_gh, 'conf/goals.csv')
 
+## if statements in case this is an OHI+ prep repo without these files
 if (RCurl::url.exists(scores_csv)) scores <- readr::read_csv(scores_csv)
 if (RCurl::url.exists(layers_csv)) layers <- readr::read_csv(layers_csv)
 if (RCurl::url.exists(conf_csv))   weight <- readr::read_csv(conf_csv) %>%
