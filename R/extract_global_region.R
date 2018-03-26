2891dd7dd024aca7223f963276efc3d1f0061c26
jules32 <jstewart@nceas.ucsb.edu>
2018-03-26 15:38:49 -0700

    build Mel's region_extract into the ohirepos package

---
+++ R/extract_global_region.R
@@ -0,0 +1,33 @@
+#' Extract global region (without subregions)
+#' Extract global regions, to be used in Tier 1 assessments
+#' @param repo_registry data frame with information about the repo
+#' @param save_dir directory to save the shapefile
+#'
+#' @return
+#' @export
+#'
+#' @examples
+extract_global_region <- function(repo_registry,
+                                  dir_save) {
+
+  ## create variables
+  rgn_name_global <- repo_registry$rgn_name_global
+
+  ## extract if regions object exists
+  if(exists("regions")) {
+
+  ## filter the regions you want
+  regions_extract <- regions[regions$rgn_name == rgn_name_global, ]
+
+  ## plot to make sure this is what you want
+  plot(regions_extract[, 1])
+
+  ## save the output (saving as an ESRI Shapefile):
+  sf::write_sf(regions_extract,
+               file.path(dir_save, paste0(rgn_name_global, ".shp")))
+
+  } else {
+    print('please source `spatial_common.R` first, you will need Mazu privileges')
+    print('source("https://raw.githubusercontent.com/OHI-Science/ohiprep_v2018/master/src/R/spatial_common.R")')
+  }
+}
